b4087dc7cfe3d7e9005b4d2b961c2f93ec8aa86e
dweslynch-unc <54442994+dweslynch-unc@users.noreply.github.com>
2019-10-25 09:25:27 -0400

    Update FizzBuzz.java

--- src/code/FizzBuzz.java
+++ src/code/FizzBuzz.java
@@ -2,7 +2,16 @@ package code;
 
 public class FizzBuzz {
 	public static String fizzBuzz(int input) {
-		return "";
+		if (input % 5 == 0)
+		{
+			if (input % 3 == 0)
+				return "FizzBuzz";
+			else
+				return "Buzz";
+		}
+		else if (input % 3 == 0)
+			return "Fizz";
+		else return Ingeger.toString(input);
 	}
 
 	public static String fizzBuzz2(int input) {
